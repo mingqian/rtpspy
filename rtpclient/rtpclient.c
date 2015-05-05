99e5124574d531dffee35d4d7e4175ebcb224769
Rafeal Han <mq_han@hotmail.com>
2015-05-05 21:06:52 +0800

    Add license headers to C files.

--- rtpclient/rtpclient.c
+++ rtpclient/rtpclient.c
@@ -1,4 +1,6 @@
- /* Author: Rafael Han
+/*
+ * Copyright (c) 2015 Rafael Han
+ * Author: Rafael Han
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy of
  * this software and associated documentation files (the "Software"), to deal in
@@ -17,6 +19,7 @@
  * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
 
 #include <stdio.h>
 #include <stdint.h>
