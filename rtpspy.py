97f954db308123640d451e45bf4d67c8e1bf43d9
Rafeal Han <mq_han@hotmail.com>
2015-05-07 20:30:03 +0800

    Update all source files with GPLv2 license header.

--- rtpspy.py
+++ rtpspy.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright (C) 2015  Rafael Han
 # Author: Rafael Han
 # 
@@ -13,9 +15,6 @@
 # 
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-#!/usr/bin/env python
 
 'RtpSpy - A RTP investigation tool'
 
